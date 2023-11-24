b0c651ab534674b0a720d884d71a95ffc187255d
Mitchel Myers <doublemx2@gmail.com>
2023-11-23 20:53:59 -0500

    Super simple hash router

---
+++ hash-router.js
@@ -0,0 +1,34 @@
+
+const router = (routeMap, template404 = () => []) => {
+    return new StandardEffect((node, destroy) => {
+        let currentDestroyer
+        destroy(() => currentDestroyer?.())
+
+        function syncHash() {
+            let hashLocation = document.location.hash.split('#')[1]
+
+            if (!hashLocation) {
+                hashLocation = '/';
+            }
+
+            let next
+
+            if (!(Object.prototype.hasOwnProperty.call(routeMap, hashLocation))) {
+                next = template404(hashLocation)
+            } else {
+                next = (0, routeMap[hashLocation])()
+            }
+
+            currentDestroyer?.()
+
+            currentDestroyer = makeDestroyer()
+
+            render(node, next, currentDestroyer.destroy)
+
+        }
+        syncHash()
+
+        window.addEventListener("hashchange", syncHash)
+        destroy(() => window.removeEventListener("hashchange", syncHash))
+    })
+}
